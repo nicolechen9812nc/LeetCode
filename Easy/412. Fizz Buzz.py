7c65c8e53afd7455a25afd0bb10c95a3457b9b27
Nicole Chen <107505783+nicolechen9812nc@users.noreply.github.com>
2023-07-11 15:16:27 -0400

    perf(412): Fizz Buzz

--- Easy/412. Fizz Buzz.py
+++ Easy/412. Fizz Buzz.py
@@ -18,3 +18,19 @@ class Solution(object):
             else:
                 answer[i]=str(answer[i])
         return(answer)
+
+#better speed + memory usage
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        answer=[]
+        for i in range(1,n+1):
+            if i%15==0:
+                answer.append("FizzBuzz")
+            elif i%5==0:
+                answer.append("Buzz")
+            elif i%3==0:
+                answer.append("Fizz")
+            else:
+                answer.append(str(i))
+        return(answer)
