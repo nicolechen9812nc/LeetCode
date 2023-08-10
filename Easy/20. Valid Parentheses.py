e33225b368d7bd19edbeec7f0a539f33c1feef71
Nicole Chen <107505783+nicolechen9812nc@users.noreply.github.com>
2023-08-10 09:44:39 -0400

    feat(20): Valid Parentheses

---
+++ Easy/20. Valid Parentheses.py
@@ -0,0 +1,24 @@
+#Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+#An input string is valid if:
+
+#Open brackets must be closed by the same type of brackets.
+#Open brackets must be closed in the correct order.
+#Every close bracket has a corresponding open bracket of the same type.
+
+
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        pairs = {
+            '(': ')',
+            '{': '}',
+            '[': ']'
+        }
+        for bracket in s:
+            if bracket in pairs:
+                stack.append(bracket)
+            elif len(stack) == 0 or bracket != pairs[stack.pop()]:
+                return False
+
+        return len(stack) == 0
